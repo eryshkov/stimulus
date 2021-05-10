dfa31bb8c87bf6a3c188d1494161d972e948252c
Evgenii Ryshkov <eryshkov@gmail.com>
2021-05-10 21:10:34 +0300

    11 - deselect color

--- assets/controllers/color-square_controller.js
+++ assets/controllers/color-square_controller.js
@@ -1,12 +1,39 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-    static targets = ['colorSquare'];
+    static targets = ['colorSquare', 'select'];
+
+    selectedColorId = null;
+
+    connect() {
+        this.selectTarget.classList.add('d-none');
+    }
 
     selectColor(event) {
+        this.setSelectColor(event.currentTarget.dataset.colorId);
+    }
+
+    setSelectColor(clickedColorId) {
+        if (clickedColorId === this.selectedColorId) {
+            this.findSelectedColorSquare().classList.remove('selected');
+            this.selectedColorId = null;
+            this.selectTarget.value = '';
+            return;
+        }
+
+        this.selectedColorId = clickedColorId;
+
         this.colorSquareTargets.forEach((element) => {
             element.classList.remove('selected');
         });
-        event.currentTarget.classList.add('selected');
+        this.findSelectedColorSquare().classList.add('selected');
+        this.selectTarget.value = this.selectedColorId;
+    }
+
+    /**
+     * @returns {Element | null}
+     */
+    findSelectedColorSquare() {
+        return this.colorSquareTargets.find((element) => element.dataset.colorId === this.selectedColorId);
     }
 }
